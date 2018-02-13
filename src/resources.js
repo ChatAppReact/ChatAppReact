5af2f3f95624abc7c106a6247015a4e1fa2e5396
Kirisanth Subramaniam <kirisanth.subramaniam@digiflare.com>
2018-02-12 23:44:17 -0500

    Updates

--- src/resources.js
+++
@@ -1,7 +0,0 @@
-class Resources {
-    static MESSAGE1 = 'hey';
-    static MESSAGE2 = 'the gift card sounds sweet i could really use the money';
-    static MESSAGE3 = 'Chat has ended. This window will now close.';
-}
-
-export default Resources;
